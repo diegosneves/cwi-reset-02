968c377bf2c5a07fd55155ad5e96a59adb1aeb93
diegosneves <neves.diegoalex@outlook.com>
2021-02-13 12:00:29 -0300

    Chore: Inserido outra linha na classe Aplicação para exibir os dados do segundo filme (Gerra ao Terror)

--- Aula02/exercicios-aula-02/src/exercicios01/Aplicacao.java
+++ Aula02/exercicios-aula-02/src/exercicios01/Aplicacao.java
@@ -7,6 +7,13 @@ public class Aplicacao {
         Filme avatar = new Filme("Avatar", "Ficçao Cientifica", 160, 2012, diretor);
         Filme gerraAoTerror = new Filme("Gerra ao Terror", "Ação", 140, 2014, diretor);
 
+        System.out.println("=====================================");
         System.out.println(avatar.toString());
+        System.out.println("=====================================");
+
+        System.out.println("=====================================");
+        System.out.println(gerraAoTerror.toString());
+        System.out.println("=====================================");
+
     }
 }
