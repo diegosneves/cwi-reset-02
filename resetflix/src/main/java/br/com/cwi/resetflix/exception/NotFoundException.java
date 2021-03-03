033be0d04df252d561dfa9080261abb14837b547
diegosneves <neves.diegoalex@outlook.com>
2021-03-03 14:33:28 -0300

    Fix: Alterado a classe NotFoundException para retornar corretamente o valor do erro para o Postman (conforme visto na aula 04)

--- resetflix/src/main/java/br/com/cwi/resetflix/exception/NotFoundException.java
+++ resetflix/src/main/java/br/com/cwi/resetflix/exception/NotFoundException.java
@@ -1,15 +1,13 @@
 package br.com.cwi.resetflix.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class NotFoundException extends HttpStatusCodeException {
-
-    private static final long serialVersionUID = -4560532949002428309L;
-
-    static HttpStatus status = HttpStatus.NOT_FOUND;
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(final String statusText) {
-        super(status, statusText);
+        super(statusText);
     }
+    
 }
